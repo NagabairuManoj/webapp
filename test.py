6017dab6d3eaa9f30a4cdb5e71a735623cff884f
manu <manojnagabairu@gmail.com>
2023-02-16 17:58:16 +0530

    Basic code

---
+++ test.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask("test")
+
+@app.route("/welcome")
+def welcome():
+    return "welcome to the flask"
+@app.route("/data")
+def data():
+    return "this is data we have"
