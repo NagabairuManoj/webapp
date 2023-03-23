96c6178492f5a3c97cd1ff577c26fa078e3f2014
NagabairuManoj <manojnagabairu@gmail.com>
2023-03-23 15:18:21 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,2 @@
 import os
-os.system("python -m flask run")
-from flask import Flask
-
-app = Flask("test")
-
-@app.route("/welcome")
-def welcome():
-    return "welcome to the flask"
-@app.route("/data")
-def data():
-    return "this is data we have"
+os.system("date")
